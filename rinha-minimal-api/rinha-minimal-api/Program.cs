5e898cfa79d8795bb33192e2d189b4d3e2861bb2
MatheusCyrillo <marc.cyrillo@gmail.com>
2023-11-16 20:08:59 -0300

    criando um container com gatling para realizar o stress test, pois rodando na minha maquina algumas conexoes estavam se perdendo o q estava impossibilitando realizar o stress test

--- rinha-minimal-api/rinha-minimal-api/Program.cs
+++ rinha-minimal-api/rinha-minimal-api/Program.cs
@@ -14,73 +14,81 @@ services.AddScoped<IPessoaRepository, PessoaRepository>();
 
 var app = builder.Build();
 
-{
-    using var scope = app.Services.CreateScope();
-    var context = scope.ServiceProvider.GetRequiredService<DataContext>();
-    await context.Init();
-}
+//{
+//    using var scope = app.Services.CreateScope();
+//    var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+//    await context.Init();
+//}
 // Configure the HTTP request pipeline.
 
 //app.UseHttpsRedirection();
 
 #region Endpoints
 
-app.MapPost("/pessoas", async (Pessoa pessoa, IPessoaRepository pessoaRepository) =>
+app.MapPost("/pessoas", async (Pessoa pessoa/*, IPessoaRepository pessoaRepository*/) =>
 {
-    pessoa.Id = Guid.NewGuid();
-    var isSuccess = await pessoaRepository.Create(pessoa);
+    //pessoa.Id = Guid.NewGuid();
+    return Results.Created($"/pessoas/{pessoa.Id}", pessoa);
 
-    if (!isSuccess)
-        return Results.UnprocessableEntity();
+    //var isSuccess = await pessoaRepository.Create(pessoa);
+
+    //if (!isSuccess)
+    //    return Results.UnprocessableEntity();
 
     return Results.Created($"/pessoas/{pessoa.Id}", pessoa);
-})
-    .AddEndpointFilter(async (invocationContext, next)
-=>
-{
-    var pessoa = invocationContext.GetArgument<Pessoa>(0);
-
-    if (string.IsNullOrEmpty(pessoa.Apelido)
-    || string.IsNullOrEmpty(pessoa.Nome)
-    || string.IsNullOrEmpty(pessoa.Nascimento)
-    || pessoa.Apelido.Length > 32
-    || pessoa.Nome.Length > 1000
-    || !DateTime.TryParse(pessoa.Nascimento, out _)
-    || (pessoa.Stack != null && pessoa.Stack.Any(s => string.IsNullOrEmpty(s) || s.Length > 32)))
-        return Results.BadRequest();
-
-    return await next(invocationContext);
 });
+//    .AddEndpointFilter(async (invocationContext, next)
+//=>
+//{
+//    //var pessoa = invocationContext.GetArgument<Pessoa>(0);
 
-app.MapGet("/pessoas/{id}", async (Guid id, IPessoaRepository pessoaRepository) =>
+//    //if (string.IsNullOrEmpty(pessoa.Apelido)
+//    //|| string.IsNullOrEmpty(pessoa.Nome)
+//    //|| string.IsNullOrEmpty(pessoa.Nascimento)
+//    //|| pessoa.Apelido.Length > 32
+//    //|| pessoa.Nome.Length > 1000
+//    //|| !DateTime.TryParse(pessoa.Nascimento, out _)
+//    //|| (pessoa.Stack != null && pessoa.Stack.Any(s => string.IsNullOrEmpty(s) || s.Length > 32)))
+//    //    return Results.BadRequest();
+
+//    return await next(invocationContext);
+//});
+
+app.MapGet("/pessoas/{id}", async (Guid id/*, IPessoaRepository pessoaRepository*/) =>
 {
-    var pessoa = await pessoaRepository.Get(id);
+    return Results.Ok();
 
-    if (pessoa == null)
-        return Results.NotFound();
+    //var pessoa = await pessoaRepository.Get(id);
 
-    return Results.Ok(pessoa);
+    //if (pessoa == null)
+    //    return Results.NotFound();
+
+    //return Results.Ok(pessoa);
 })
     .WithName("GetPessoaById");
 
-app.MapGet("/pessoas", async (string t, IPessoaRepository pessoaRepository) =>
+app.MapGet("/pessoas", async (string t/*, IPessoaRepository pessoaRepository*/) =>
 {
-    return Results.Ok(await pessoaRepository.GetPessoasByTermo(t));
-})
-    .AddEndpointFilter(async (invocationContext, next)
-=>
-    {
-        var termo = invocationContext.GetArgument<string>(0);
+    return Results.Ok();
 
-        if (termo == null)
-            return Results.BadRequest();
+    //return Results.Ok(await pessoaRepository.GetPessoasByTermo(t));
+});
+//    .AddEndpointFilter(async (invocationContext, next)
+//=>
+//    {
+//        var termo = invocationContext.GetArgument<string>(0);
 
-        return await next(invocationContext);
-    });
+//        if (termo == null)
+//            return Results.BadRequest();
 
-app.MapGet("/contagem-pessoas", async (IPessoaRepository pessoaRepository) =>
+//        return await next(invocationContext);
+//    });
+
+app.MapGet("/contagem-pessoas", async (/*IPessoaRepository pessoaRepository*/) =>
 {
-    return await pessoaRepository.TotalCount();
+    return Results.Ok(0);
+
+    //return Results.Ok(await pessoaRepository.TotalCount());
 });
 
 #endregion
